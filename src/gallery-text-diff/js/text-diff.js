e893efc1e394d69b5ec6867135c87569c0fb358a
Alain Horner <alain.horner@liip.ch>
2012-02-06 16:54:34 +0100

    Dont wrap the component with YUI.add, since the builder will do this

--- src/gallery-text-diff/js/text-diff.js
+++ src/gallery-text-diff/js/text-diff.js
@@ -10,25 +10,154 @@
  * of the string, the actions would have to take place. The characters used for the different actions are configurable
  *
  */
-YUI.add('text-diff', function (Y) {
 
-    'use strict';
+'use strict';
+
+/****************************************************************************************/
+/************************************ public members ************************************/
+/****************************************************************************************/
+
+Y.TextDiff = Y.Base.create('TextDiff', Y.Base, [], {
 
     /****************************************************************************************/
-    /************************************* constructor **************************************/
+    /*********************************** private methods ************************************/
     /****************************************************************************************/
 
-    function TextDiff(config) {
-        TextDiff.superclass.constructor.apply(this, arguments);
+    /**
+     * Creates a string of variable length, that consists of one kind of character
+     *
+     * @param {Char} letter     The character the string consists of
+     * @param {Number} length   The length of the string
+     * @return {String}
+     **/
+    _constructSingleCharString: function(letter, length) {
+        var singleCharString = '',
+            i;
+
+        for (i = 0; i < length; i++) {
+            singleCharString += letter;
+        }
+
+        return singleCharString;
+    },
+
+    /****************************************************************************************/
+    /************************************ public methods ************************************/
+    /****************************************************************************************/
+
+    /**
+     * Calculates the diff between two strings
+     *
+     * @param {String} targetStr    The target string
+     * @param {String} compStr      The comparison string
+     * @return {Object}             An object with two properties:
+     *                              - {String}  diffString  A string describing the difference between the two given strings
+     *                              - {Number}  dist        The Damerau-Levenshtein distance which can be interpreted as the amount of differences
+     **/
+    calculateDiff: function(targetStr, compStr) {
+        var instance = this,
+            targetArray = null,
+            compArray = null,
+            diffMatrix = [],
+            minObj = null,
+            i,
+            j;
+
+        // trivial cases
+        if (targetStr === compStr) {
+            return {
+                diffString: this._constructSingleCharString(this.get('complianceChar'), targetStr.length),
+                dist: 0
+            }
+        } else if (compStr.length === 0) {
+            return {
+                diffString: this._constructSingleCharString(this.get('insertionChar'), targetStr.length),
+                dist: targetStr.length
+            }
+        } else if (targetStr.length === 0) {
+            return {
+                diffString: this._constructSingleCharString(this.get('deletionChar'), compStr.length),
+                dist: compStr.length
+            }
+        }
+
+        // prepare matrix and comparison-vectors
+        targetArray = targetStr.split('');
+        compArray = compStr.split('');
+
+        for (i = 0; i <= compStr.length; i++) {
+            diffMatrix[i] = [{
+                dist: i,
+                diff: this._constructSingleCharString(this.get('deletionChar'), i)
+            }];
+        }
+
+        for (j = 0; j <= targetStr.length; j++) {
+            diffMatrix[0][j] = {
+                dist: j,
+                diff: this._constructSingleCharString(this.get('insertionChar'), j)
+            }
+        }
+
+        // calculate difference
+        Y.Array.each(compArray, function (compChar, i) {
+            Y.Array.each(targetArray, function (targetChar, j) {
+                if (compArray[i] === targetArray[j]) {
+
+                    // compliance
+                    diffMatrix[i+1][j+1] = {
+                        dist: diffMatrix[i][j]['dist'],
+                        diff: diffMatrix[i][j]['diff'] + instance.get('complianceChar')
+                    }
+
+                } else {
+
+                    // deletion
+                    minObj = {
+                        dist: diffMatrix[i][j+1]['dist'] + 1,
+                        diff: diffMatrix[i][j+1]['diff'] + instance.get('deletionChar')
+                    };
+
+                    // insertion
+                    if (diffMatrix[i+1][j]['dist'] < minObj['dist']) {
+                        minObj = {
+                            dist: diffMatrix[i+1][j]['dist'] + 1,
+                            diff: diffMatrix[i+1][j]['diff'] + instance.get('insertionChar')
+                        };
+                    }
+
+                    // substitution
+                    if (diffMatrix[i][j]['dist'] < minObj['dist']) {
+                        minObj = {
+                            dist: diffMatrix[i][j]['dist'] + 1,
+                            diff: diffMatrix[i][j]['diff'] + instance.get('substitutionChar')
+                        };
+                    }
+
+                    // transposition
+                    if(i > 0 && j > 0 && compChar == targetArray[j-1] && targetChar == compArray[i-1] && diffMatrix[i-1][j-1]['dist'] < minObj['dist']) {
+                        minObj = {
+                            dist: diffMatrix[i-1][j-1]['dist'] + 1,
+                            diff: diffMatrix[i-1][j-1]['diff'] + instance.get('transpositionChar') + instance.get('transpositionChar')
+                        };
+                    }
+
+                    diffMatrix[i+1][j+1] = minObj;
+
+                }
+            });
+        });
+
+        return {
+            diffString: diffMatrix[compStr.length][targetStr.length]['diff'],
+            dist: diffMatrix[compStr.length][targetStr.length]['dist']
+        }
+
     }
 
-    TextDiff.NAME = 'textDiff';
-
-    /****************************************************************************************/
-    /************************************ public members ************************************/
-    /****************************************************************************************/
-
-    TextDiff.ATTRS = {
+}, {
+    NS: 'text-diff',
+    ATTRS: {
         complianceChar: {
             value: '_'
         },
@@ -44,148 +173,5 @@ YUI.add('text-diff', function (Y) {
         transpositionChar: {
             value: 't'
         }
-    };
-
-    Y.extend(TextDiff, Y.Base, {
-
-        /****************************************************************************************/
-        /*********************************** private methods ************************************/
-        /****************************************************************************************/
-
-        /**
-         * Creates a string of variable length, that consists of one kind of character
-         *
-         * @param {Char} letter     The character the string consists of
-         * @param {Number} length   The length of the string
-         * @return {String}
-         **/
-        _constructSingleCharString: function(letter, length) {
-            var singleCharString = '',
-                i;
-
-            for (i = 0; i < length; i++) {
-                singleCharString += letter;
-            }
-
-            return singleCharString;
-        },
-
-        /****************************************************************************************/
-        /************************************ public methods ************************************/
-        /****************************************************************************************/
-
-        /**
-         * Calculates the diff between two strings
-         *
-         * @param {String} targetStr    The target string
-         * @param {String} compStr      The comparison string
-         * @return {Object}             An object with two properties:
-         *                              - {String}  diffString  A string describing the difference between the two given strings
-         *                              - {Number}  dist        The Damerau-Levenshtein distance which can be interpreted as the amount of differences
-         **/
-        calculateDiff: function(targetStr, compStr) {
-            var instance = this,
-                targetArray = null,
-                compArray = null,
-                diffMatrix = [],
-                minObj = null,
-                i,
-                j;
-
-            // trivial cases
-            if (targetStr === compStr) {
-                return {
-                    diffString: this._constructSingleCharString(this.get('complianceChar'), targetStr.length),
-                    dist: 0
-                }
-            } else if (compStr.length === 0) {
-                return {
-                    diffString: this._constructSingleCharString(this.get('insertionChar'), targetStr.length),
-                    dist: targetStr.length
-                }
-            } else if (targetStr.length === 0) {
-                return {
-                    diffString: this._constructSingleCharString(this.get('deletionChar'), compStr.length),
-                    dist: compStr.length
-                }
-            }
-
-            // prepare matrix and comparison-vectors
-            targetArray = targetStr.split('');
-            compArray = compStr.split('');
-
-            for (i = 0; i <= compStr.length; i++) {
-                diffMatrix[i] = [{
-                    dist: i,
-                    diff: this._constructSingleCharString(this.get('deletionChar'), i)
-                }];
-            }
-
-            for (j = 0; j <= targetStr.length; j++) {
-                diffMatrix[0][j] = {
-                    dist: j,
-                    diff: this._constructSingleCharString(this.get('insertionChar'), j)
-                }
-            }
-
-            // calculate difference
-            Y.Array.each(compArray, function (compChar, i) {
-                Y.Array.each(targetArray, function (targetChar, j) {
-                    if (compArray[i] === targetArray[j]) {
-
-                        // compliance
-                        diffMatrix[i+1][j+1] = {
-                            dist: diffMatrix[i][j]['dist'],
-                            diff: diffMatrix[i][j]['diff'] + instance.get('complianceChar')
-                        }
-
-                    } else {
-
-                        // deletion
-                        minObj = {
-                            dist: diffMatrix[i][j+1]['dist'] + 1,
-                            diff: diffMatrix[i][j+1]['diff'] + instance.get('deletionChar')
-                        };
-
-                        // insertion
-                        if (diffMatrix[i+1][j]['dist'] < minObj['dist']) {
-                            minObj = {
-                                dist: diffMatrix[i+1][j]['dist'] + 1,
-                                diff: diffMatrix[i+1][j]['diff'] + instance.get('insertionChar')
-                            };
-                        }
-
-                        // substitution
-                        if (diffMatrix[i][j]['dist'] < minObj['dist']) {
-                            minObj = {
-                                dist: diffMatrix[i][j]['dist'] + 1,
-                                diff: diffMatrix[i][j]['diff'] + instance.get('substitutionChar')
-                            };
-                        }
-
-                        // transposition
-                        if(i > 0 && j > 0 && compChar == targetArray[j-1] && targetChar == compArray[i-1] && diffMatrix[i-1][j-1]['dist'] < minObj['dist']) {
-                            minObj = {
-                                dist: diffMatrix[i-1][j-1]['dist'] + 1,
-                                diff: diffMatrix[i-1][j-1]['diff'] + instance.get('transpositionChar') + instance.get('transpositionChar')
-                            };
-                        }
-
-                        diffMatrix[i+1][j+1] = minObj;
-
-                    }
-                });
-            });
-
-            return {
-                diffString: diffMatrix[compStr.length][targetStr.length]['diff'],
-                dist: diffMatrix[compStr.length][targetStr.length]['dist']
-            }
-
-        }
-
-    });
-
-    Y.namespace('elHornair').TextDiff = TextDiff;
-
-}, '0.1', {requires: ['base']});
+    }
+});
